e79481a6120da33130fb1f1f1a407db49accd1b1
mkceliks <66789068+mkceliks@users.noreply.github.com>
2022-03-25 21:57:28 +0300

    Version 2.0 with 'Doughnut'.
    Bar Chart kısmı silindi yerine 'Doughnut' chart'ı eklendi. Yorum satırları türkçeleştirildi daha açık olması adına. Mobil Responsive'lik yapıldı. Doughnut ortasına text enjekte edildi.

--- scripts/button_1.js
+++ scripts/button_1.js
@@ -1,7 +1,7 @@
 document.getElementById("myBtn1").onclick = function() {myFunction1()};
 function myFunction1() {
   if (document.getElementById("table1").style.display === "none") {
-    //table1.display becames block type from none here.
+    //table1.display Değişikliği
    document.getElementById("table1").style.display = "block";
    document.getElementById("table2").style.display = "none";
    document.getElementById("table2_header").style.display = "none";
